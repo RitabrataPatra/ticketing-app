7a2dc911a87734bacf3ce7eb33f246452e8c37a7
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-12 16:51:33 +0530

    created a get function to fetch api from DB

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
-
 import React from 'react'
 import { TicketCard } from "./(components)/TicketCard";
 
+
+const getTickets = async()=>{
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets" , {
+      cache : "no-store"
+    })
+    return res.json()
+  } catch (error) {
+      console.log("Failed to get tickets", error);
+  }
+  
+}
+
 const Dashboard = () => {
+  
   return (
     <div className="p-5">
       <div className="lg:grid grid-cols-2 xl:grid-cols-4 gap-4">
