f60109bd7e0d0f9bcb79af0328a78e7b0e719128
Ritabrata Patra <teamritabrata@gmail.com>
2024-12-13 16:43:50 +0530

    now the update button works properly

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -35,28 +35,30 @@ export const TicketCard = ({ ticket }) => {
       {/* for updating the card right click on it */}
       <ContextMenu>
         <ContextMenuTrigger>
-          <Link href="/">
-            <h4>{ticket.title}</h4>
-            <hr className="h-px border-0 bg-page mb-2 " />
-            <p className="whitspace-pre-wrap">{ticket.description}</p>
+          <h4>{ticket.title}</h4>
+          <hr className="h-px border-0 bg-page mb-2 " />
+          <p className="whitspace-pre-wrap">{ticket.description}</p>
 
-            <div className="flex-grow">
-              <div className="flex mt-2 ">
-                <div className="flex flex-col">
-                  <p className="text-xs my-1">
-                    {currentDate} at {currentTime}
-                  </p>
-                  <ProgressBar progress={ticket.progress} />
-                </div>
-                <div className="ml-auto flex items-end">
-                  <StatusDisplay status={ticket.status} />
-                </div>
+          <div className="flex-grow">
+            <div className="flex mt-2 ">
+              <div className="flex flex-col">
+                <p className="text-xs my-1">
+                  {currentDate} at {currentTime}
+                </p>
+                <ProgressBar progress={ticket.progress} />
+              </div>
+              <div className="ml-auto flex items-end">
+                <StatusDisplay status={ticket.status} />
               </div>
             </div>
-          </Link>
+          </div>
         </ContextMenuTrigger>
-        <ContextMenuContent  className="w-64 backdrop-blur-xl bg-transparent border border-slate-800">
-            <ContextMenuItem inset className="text-white hover:cursor-pointer ">Update</ContextMenuItem>
+        <ContextMenuContent className="w-64 backdrop-blur-xl bg-transparent border border-slate-800">
+          <Link href="/">
+            <ContextMenuItem inset className="text-white hover:cursor-pointer ">
+              Update
+            </ContextMenuItem>
+          </Link>
         </ContextMenuContent>
       </ContextMenu>
     </div>
